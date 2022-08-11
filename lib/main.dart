5ff0799cea7cb36882a9250f6b4b98ae519ec7ca
fernandomgv <fernando.mogollon@iica.int>
2022-08-10 22:10:07 -0500

    He modificado el titulo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'StateApp',
+      title: 'Singleton StateApp',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
